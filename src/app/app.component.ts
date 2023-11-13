ca981a725421c97232f537a16cca4fa7afbd93d4
Gerardo Gastelum <gegage98@gmail.com>
2023-11-13 10:57:16 -0600

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TestTecnicoAdsum';
+
+  contactForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private httpClient: HttpClient) {
+    this.contactForm = this.formBuilder.group({
+      nombreCompleto: '',
+      nombreEmpresa: '',
+      correoElectronico: '',
+      telefono: '',
+      categoria: '',
+      mensaje: ''
+    });
+  }
+
+  onSubmit() {
+    const formData = this.contactForm.value;
+    const jsonFormData = JSON.stringify(formData);
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+    console.log(jsonFormData);
+    this.httpClient.post('https://localhost:7297/api/contacto', jsonFormData, { headers }).subscribe(
+      (response) => {
+        console.log('Solicitud enviada con éxito', response);
+      },
+      (error) => {
+        console.error('Error al enviar la solicitud', error);
+      }
+    );
+  }
 }
